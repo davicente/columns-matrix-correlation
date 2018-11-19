7e6c95e8f45c0d68e8ed6b2faddca938564ac5f7
709849 <David.VicenteFuentes@cognizant.com>
2018-11-19 17:31:04 +0100

    Unit tests

---
+++ test/test.js
@@ -0,0 +1,31 @@
+const expect = require('chai').expect;
+const assert = require('chai').assert;
+
+const correlation = require('../index.js');
+
+const matrix = [ 
+  [1, 4, 118, 70, 0, 0, 44.5],
+  [2, 2, 122, 76, 27, 200, 35.9],
+  [3, 6, 125, 78, 31, 0, 27.6],
+  [4, 1, 168, 88, 29, 0, 35.0],
+  [5, 2, 129, 0, 0, 0, 38.5]
+];
+
+
+
+describe('Matrix columns linear correlation', () => {
+  it('Success', () => {
+    const correlationResults = correlation.calculateCorrelation(matrix);
+    // num of rows in result has to be same than number of columns of original matrix
+    assert.strictEqual(correlationResults.length, matrix[0].length);
+    // num of columns in result has to be same than number of columns of original matrix
+    assert.strictEqual(correlationResults[0].length, matrix[0].length);
+    
+    for(let column of correlationResults) {
+      for(let value of column) {
+        console.log(value);
+        expect(value).to.be.within(-1.000001, 1.0000001); 
+      }
+    }
+  });
+});
